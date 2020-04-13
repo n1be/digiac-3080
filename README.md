7239ab2d510347ddc4e0fdc3da36322dc8ac5606
Rob Evans <n1be@bithub>
2020-04-12 22:07:07 -0400

    Finish supplying paper tapes
    - provide tape/stok.ptp in addition to tape/stok_no-randomize.ptp
    - fix tape_dump.py to cleanly exit when piped to head
    - wordsmith Digiac info.odt, README.md, tape/papertape_info.odt

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# digiac-3080
+# Digiac-3080
 
 The [Digiac-3080](https://digiac3080.wordpress.com/ "Extinct Computer Tribute Blog")
 was the first computer architecture I learned.  This simple slow serial machine
-with no interrupts or stack could still educate and entertain.  I saved one program
-from those days and wrote an emulator to be able to run it again.
+with no interrupts or stack could still educate and entertain.  I saved one
+program, a Stock Market game, from those days and wrote an emulator to be able
+to run the game again.
+
+### Documentation Files
+These files explain the Digiac and how to run the emulator.
+- Digiac info.pdf - Details of the Digiac-3080 machine architecture.
+- tape/papertape_info.pdf - Explains the format of Digiac paper tapes
 
 
--- tape_dump.py
+++ tape_dump.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 "Dump Digiac paper tape"
-from sys import argv
+from sys import argv, exit
 
 
 def ta_char(b):
@@ -14,46 +14,50 @@ def ta_char(b):
     return (alph + beta)[b]
 
 
-for tape in argv[1:]:
-    print()
-    buf = bytes(0)
-    with open(tape, "rb") as f:
-        b = True
-        while b:
-            b = f.read()
-            buf += b
-    ln = len(buf)
-    print(f"File: {tape} raw data length: {ln}")
-    while buf:
-        addr = 0
-        buf = buf.lstrip(b"\x00")
-        l = ln - len(buf)
-        if not buf:
-            print(f"removed final {l} zero bytes")
-            break
-        print(
-            f"""removed {l} leading zero bytes
+try:
+    files = argv[1:]
+    if not files:
+        exit("Supply the filename(s) to be dumped on the command line")
+    for tape in argv[1:]:
+        print()
+        buf = bytes(0)
+        with open(tape, "rb") as f:
+            b = True
+            while b:
+                b = f.read()
+                buf += b
+        ln = len(buf)
+        print(f"File: {tape} raw data length: {ln}")
+        while buf:
+            addr = 0
+            buf = buf.lstrip(b"\x00")
+            l = ln - len(buf)
+            if not buf:
+                print(f"removed final {l} zero bytes")
+                break
+            print(
+                f"""removed {l} leading zero bytes
   Addr    Octal    Char
   ====  ========   ===="""
-        )
-        while buf[0] != 0:
-            assert buf[0] == 0x40, f'{buf[0]:02X}h =>> "-"?'
-            assert buf[1] != 0, f" == {buf[1]}"
-            assert buf[2] != 0, f" == {buf[2]}"
-            assert buf[3] != 0, f" == {buf[3]}"
-            assert buf[4] != 0, f" == {buf[4]}"
-            b1 = buf[1] % 64
-            b2 = buf[2] % 64
-            b3 = buf[3] % 64
-            b4 = buf[4] % 64
-            c1 = ta_char(b1)
-            c2 = ta_char(b2)
-            c3 = ta_char(b3)
-            c4 = ta_char(b4)
-            wd = ((b1 * 64 + b2) * 64 + b3) * 64 + b4
-            print(f". {addr:04o}  {wd:08o}  |{c1}{c2}{c3}{c4}|")
-            addr += 1
-            buf = buf[5:]
-        ln = len(buf)
-else:
-    print(f"Supply the filename(s) to be dumped on the command line")
+            )
+            while buf[0] != 0:
+                assert buf[0] == 0x40, f'{buf[0]:02X}h =>> "-"?'
+                assert buf[1] != 0, f" == {buf[1]}"
+                assert buf[2] != 0, f" == {buf[2]}"
+                assert buf[3] != 0, f" == {buf[3]}"
+                assert buf[4] != 0, f" == {buf[4]}"
+                b1 = buf[1] % 64
+                b2 = buf[2] % 64
+                b3 = buf[3] % 64
+                b4 = buf[4] % 64
+                c1 = ta_char(b1)
+                c2 = ta_char(b2)
+                c3 = ta_char(b3)
+                c4 = ta_char(b4)
+                wd = ((b1 * 64 + b2) * 64 + b3) * 64 + b4
+                print(f". {addr:04o}  {wd:08o}  |{c1}{c2}{c3}{c4}|")
+                addr += 1
+                buf = buf[5:]
+            ln = len(buf)
+except BrokenPipeError:
+    pass
