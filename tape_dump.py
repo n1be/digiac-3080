7bfeeec25c725764a4cb0ec987b8348ed0e72373
Rob Evans <n1be@bithub>
2020-04-17 20:00:00 -0400

    Added copyright notice

--- tape_dump.py
+++ tape_dump.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+# tape_dump.py - Dump the contents of Digiac-3080 paper tape (.ptp) files
+
+#   Copyright (C) 2020 Robert N. Evans
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 "Dump Digiac paper tape"
 from sys import argv
 
